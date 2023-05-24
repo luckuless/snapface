8ac6b33a30d83b15e3b4fdf4146bd75269989f25
luckuless <luckuless.rambojun@icloud.com>
2023-05-24 15:31:41 +0200

    formulaire basique - template forms

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,10 +9,18 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent {
 
-  constructor(private router: Router){}
+  userEmail: string;
+
+  constructor(private router: Router){
+    this.userEmail = 'me@my-house.com';
+  }
 
   onContinue() {
       this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
   }
 
 
