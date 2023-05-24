ac0dfc18148d15dbd889a4d1e8ab0ddb5bfafdd1
luckuless <luckuless.rambojun@icloud.com>
2023-05-24 15:54:50 +0200

    Reactive forms + observe form object

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-new-face-snap',
+  templateUrl: './new-face-snap.component.html',
+  styleUrls: ['./new-face-snap.component.scss']
+})
+export class NewFaceSnapComponent implements OnInit {
+
+  snapForm!: FormGroup;
+
+  faceSnapPreview$!: Observable<FaceSnap>;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.snapForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null]
+    });
+
+    this.observeSnapToSeeChangesWhenTyping();
+  }
+
+  observeSnapToSeeChangesWhenTyping(): void {
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdDate: new Date(),
+        snaps: 0,
+        id: 0
+      }))
+    );
+  }
+
+  onSubmitForm() {
+    console.log(this.snapForm.value);
+  }
+
+
+}
