250ceb843623f7ef4f9f410694fed865b540da84
luckuless <luckuless.rambojun@icloud.com>
2023-05-25 14:34:35 +0200

    refactor into modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
-import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
+import { FaceSnapListComponent } from './face-snaps/face-snap-list/face-snap-list.component';
+import { LandingPageComponent } from './landing-page/landing-page-compoment/landing-page.component';
+import { SingleFaceSnapComponent } from './face-snaps/single-face-snap/single-face-snap.component';
+import { NewFaceSnapComponent } from './face-snaps/new-face-snap/new-face-snap.component';
 
 const routes: Routes = [
     { path: 'facesnaps/:id', component: SingleFaceSnapComponent },
